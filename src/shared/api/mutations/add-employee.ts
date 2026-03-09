01a7a1ada480f9d97efaaeac89ea996b6f18bc78
Nina <ninakya@mail.ru>
2026-03-09 13:04:58 +0300

    fix: fix add employee optimistic update

--- src/shared/api/mutations/add-employee.ts
+++ src/shared/api/mutations/add-employee.ts
@@ -1,5 +1,5 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { api, type Employee,  } from '../api';
+import { api, type Employee } from '../api';
 import { queryKeys } from '../query-keys';
 
 export const useAddEmployee = () => {
@@ -8,28 +8,28 @@ export const useAddEmployee = () => {
     return useMutation({
         mutationFn: async (employee: Employee) => {
             if (!employee) {
-                throw new Error('ID сотрудника не указан');
+                throw new Error('Данные сотрудника не указаны');
             }
 
             await api.addEmployee(employee);
             return employee;
         },
 
-        onMutate: async (employee) => {
+        onMutate: async (newEmployee) => {
             await queryClient.cancelQueries({ 
                 queryKey: [queryKeys.getEmployees] 
             });
 
-            const previousEmployees = queryClient.getQueryData<Employee[]>([queryKeys.getEmployees]);
+            const previousEmployees = queryClient.getQueryData<Employee[]>([queryKeys.getEmployees]);    
             queryClient.setQueryData<Employee[]>(
                 [queryKeys.getEmployees],
-                (old) => old?.filter(emp => emp !== employee)
+                (old) => old ? [newEmployee, ...old] : [newEmployee]
             );
 
             return { previousEmployees };
         },
 
-        onError: (_err, _, context) => {
+        onError: (_err, _newEmployee, context) => {
             if (context?.previousEmployees) {
                 queryClient.setQueryData(
                     [queryKeys.getEmployees], 
